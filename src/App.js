6dce03ef2a98bf7286f151db7c27756c10c977c5
David White <dwhite0627@gmail.com>
2020-02-15 22:43:21 -0500

    Add UserPage component to App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserPage from './components/UserPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserPage 
+        username={'dvwhite'}
+      />
+    </div>
+  );
+}
+
+export default App;
